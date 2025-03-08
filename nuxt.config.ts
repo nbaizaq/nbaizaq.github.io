d90a8b61cbe11fb30e1e26737d21f5705eea6a32
Nurbek Baizakov <n.baizakof@gmail.com>
2025-03-08 20:48:05 +0600

    set package manager

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -43,7 +43,7 @@ export default defineNuxtConfig({
           crossorigin: "",
         },
         {
-          href: "https://fonts.googleapis.com/css2?family=RobotoMono:ital,wght@0,100..900;1,100..900&display=swap",
+          href: "https://fonts.googleapis.com/css2?family=Noto+Sans+Mono:wght@100..900&family=Roboto+Mono:ital,wght@0,100..700;1,100..700&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap",
           rel: "stylesheet",
         },
       ],
